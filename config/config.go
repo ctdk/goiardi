87b58af6410dc943d97a1fc029cd5ee9cd8cec23
Jeremy Bingham <jbingham@gmail.com>
2014-01-06 20:59:19 -0800

    Bit of a leap, but adding freezing data to disk seems worthy of jumping to 0.3.0

--- config/config.go
+++ config/config.go
@@ -55,7 +55,7 @@ type Options struct {
 }
 
 // The goiardi version
-const Version = "0.2.1"
+const Version = "0.3.0"
 // The chef version we're at least aiming for, even if it's not complete yet
 const ChefVersion = "11.0.8"
 
