048d50821426bcce12ede88b59da7b817ab6491b
ctdk <jbingham@gmail.com>
2014-03-19 21:42:17 -0700

    Bump version for release and update CHANGELOG

--- config/config.go
+++ config/config.go
@@ -76,7 +76,7 @@ type Options struct {
 }
 
 // The goiardi version.
-const Version = "0.4.1"
+const Version = "0.4.2"
 // The chef version we're at least aiming for, even if it's not complete yet.
 const ChefVersion = "11.0.8"
 
