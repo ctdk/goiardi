e9149d12490347cbb2209fa5baa382d7d084856f
Jeremy Bingham <jbingham@gmail.com>
2014-04-28 17:37:45 -0700

    Prerelease version

--- config/config.go
+++ config/config.go
@@ -92,7 +92,7 @@ type Options struct {
 }
 
 // The goiardi version.
-const Version = "0.4.2"
+const Version = "0.4.9999"
 // The chef version we're at least aiming for, even if it's not complete yet.
 const ChefVersion = "11.0.8"
 
