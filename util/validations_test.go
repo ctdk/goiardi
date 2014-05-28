d964859d276c276498dca37262ccda45402b181c
Yasushi Abe <yasushi.abe@gmail.com>
2014-05-28 13:56:35 +0900

    Fixed run_list validation.
    Conformed to the Chef::RunList::RunListItem.

---
+++ util/validations_test.go
@@ -0,0 +1,35 @@
+package util
+
+import (
+	"testing"
+)
+
+func TestValidateRunList(t *testing.T) {
+	runList := []string{
+		"recipe[qualified_recipe_name]",
+		"recipe[versioned_qualified_recipe_name@1.0.0]",
+		"recipe[versioned_qualified_recipe_name2@1.0]",
+		"recipe[qualified_recipe_name::include.period]",
+		"recipe[versioned_qualified_recipe_name::include.period@1.0.0]",
+		"role[qualified_role_name]",
+		"versioned_unqualified_recipe_name@1.0.0",
+	}
+	rl, err := ValidateRunList(runList)
+	if err != nil {
+		t.Errorf("%v shoud have passed run list validation, but didn't.", runList)
+	}
+	t.Logf("%+v", rl)
+
+	falseFriends := []string{
+		"Recipe[recipe_name]",
+		"roles[role_name]",
+		"recipe[invalid_version@1]",
+		"recipe[invalid_version@1.2.3.4]",
+		"recipe[invalid_version@abc]",
+	}
+	for _, falseFriend := range falseFriends {
+		if _, err := ValidateRunList([]string{falseFriend}); err == nil {
+			t.Errorf("%v shoud not have passed run list validation, but somehow did", falseFriend)
+		}
+	}
+}
