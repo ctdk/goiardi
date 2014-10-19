94517624d94ba2067111a18fb25b20bb1c50ff4a
ctdk <jbingham@gmail.com>
2014-10-19 11:42:57 -0700

    report tests

--- report/report_test.go
+++ report/report_test.go
@@ -20,21 +20,27 @@ import (
 	"encoding/gob"
 	"fmt"
 	"github.com/ctdk/goiardi/node"
+	"github.com/ctdk/goiardi/organization"
 	"testing"
 	"time"
 )
 
+var org *organization.Organization
+
 func TestReportCreation(t *testing.T) {
+	gob.Register(new(organization.Organization))
+	org, _ = organization.New("default", "boo")
+	org.Save()
 	uuid := "12b8be8d-a2ef-4fc6-88b3-4c18103b88df"
 	invalidUUID := "12b8be8d-a2ef-4fc6-88b3-4c18103b88zz"
-	r, err := New(uuid, "node")
+	r, err := New(org, uuid, "node")
 	if err != nil {
 		t.Errorf(err.Error())
 	}
 	if r.RunID != uuid {
 		t.Errorf("run ids are not identical: %s :: %s", r.RunID, uuid)
 	}
-	_, err = New(invalidUUID, "node")
+	_, err = New(org, invalidUUID, "node")
 	if err == nil {
 		t.Errorf("%s created a report, but it shouldn't have", invalidUUID)
 	}
@@ -48,7 +54,7 @@ func TestReportUpdating(t *testing.T) {
 	update["resources"] = make([]interface{}, 0)
 	update["run_list"] = "[]"
 	update["data"] = make(map[string]interface{})
-	r, err := NewFromJSON("node", create)
+	r, err := NewFromJSON(org, "node", create)
 	if err != nil {
 		t.Errorf(err.Error())
 	}
@@ -64,25 +70,25 @@ func TestReportListing(t *testing.T) {
 	gob.Register(new(Report))
 	for i := 0; i < 3; i++ {
 		u := fmt.Sprintf(uuid, i)
-		r, _ := New(u, "node")
+		r, _ := New(org, u, "node")
 		r.StartTime = time.Now()
 		r.Save()
 	}
-	rs := GetList()
+	rs := GetList(org)
 	if len(rs) != 3 {
 		t.Errorf("expected 3 items in list, got %d", len(rs))
 	}
 
-	n, _ := node.New("node2")
+	n, _ := node.New(org, "node2")
 	for i := 4; i < 6; i++ {
 		u := fmt.Sprintf(uuid, i)
-		r, _ := New(u, n.Name)
+		r, _ := New(org, u, n.Name)
 		r.StartTime = time.Now()
 		r.Save()
 	}
 	from := time.Now().Add(-(time.Duration(24*90) * time.Hour))
 	until := time.Now()
-	ns, nerr := GetNodeList(n.Name, from, until, 100, "")
+	ns, nerr := GetNodeList(org, n.Name, from, until, 100, "")
 	if nerr != nil {
 		t.Errorf(nerr.Error())
 	}
@@ -90,15 +96,15 @@ func TestReportListing(t *testing.T) {
 		t.Errorf("expected 2 items from node 'node2', got %d", len(ns))
 	}
 
-	zs, rerr := GetReportList(from, until, 100, "started")
+	zs, rerr := GetReportList(org, from, until, 100, "started")
 	if rerr != nil {
 		t.Errorf(rerr.Error())
 	}
-	rs = GetList()
+	rs = GetList(org)
 	if len(zs) != len(rs) {
 		t.Errorf("Searching on 'started' status here should have returned everything but it didn't")
 	}
-	zs, rerr = GetReportList(from, until, 100, "success")
+	zs, rerr = GetReportList(org, from, until, 100, "success")
 	if rerr != nil {
 		t.Errorf(rerr.Error())
 	}
