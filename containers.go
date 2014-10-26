58ff38eac0484f4738480ed20a37ab7673317f78
ctdk <jbingham@gmail.com>
2014-10-25 23:13:17 -0700

    Stub out acl and container handlers

---
+++ containers.go
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2013-2014, Jeremy Bingham (<jbingham@gmail.com>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package main
+
+import (
+	"encoding/json"
+	//"github.com/ctdk/goiardi/user"
+	"github.com/ctdk/goiardi/organization"
+	//"github.com/ctdk/goiardi/util"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+// container handlers
+
+// may have separate handlers for each kind of container, if warranted.
+func containerHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	vars := mux.Vars(r)
+
+	orgName := vars["org"]
+	org, orgerr := organization.Get(orgName)
+	if orgerr != nil {
+		jsonErrorReport(w, r, orgerr.Error(), orgerr.Status())
+		return
+	}
+	_ = org
+
+	response := make(map[string]interface{})
+
+	enc := json.NewEncoder(w)
+	if err := enc.Encode(&response); err != nil {
+		jsonErrorReport(w, r, err.Error(), http.StatusInternalServerError)
+	}
+}
