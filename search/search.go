7deab541c279c29eddec87fce48bbcc2c27c9634
ctdk <jbingham@gmail.com>
2015-03-31 23:01:18 -0700

    Start search refactor

--- search/search.go
+++ search/search.go
@@ -26,8 +26,55 @@ import (
 	"github.com/ctdk/goiardi/indexer"
 	"github.com/ctdk/goiardi/node"
 	"github.com/ctdk/goiardi/role"
+	"github.com/ctdk/goiardi/util"
 	"net/url"
+	"reflect"
+	"strings"
+	"sort"
 )
+
+// Searcher is an interface that any search backend needs to implement. It's
+// up to the Searcher to use whatever backend it wants to return the desired
+// results.
+type Searcher interface {
+	Search(string, string, int, string, int) ([]map[string]interface{}, error)
+	GetEndpoints() []string
+}
+
+type results struct {
+	res     []map[string]interface{}
+	sortKey string
+}
+
+func (r results) Len() int      { return len(r.res) }
+func (r results) Swap(i, j int) { r.res[i], r.res[j] = r.res[j], r.res[i] }
+func (r results) Less(i, j int) bool {
+	ibase := r.res[i][r.sortKey]
+	jbase := r.res[j][r.sortKey]
+	ival := reflect.ValueOf(ibase)
+	jval := reflect.ValueOf(jbase)
+	if (!ival.IsValid() && !jval.IsValid()) || ival.IsValid() && !jval.IsValid() {
+		return true
+	} else if !ival.IsValid() && jval.IsValid() {
+		return false
+	}
+	// don't try and compare different types for now. If this ever becomes
+	// an issue in practice, though, it should be revisited
+	if ival.Type() == jval.Type() {
+		switch ibase.(type) {
+		case int, int8, int32, int64:
+			return ival.Int() < jval.Int()
+		case uint, uint8, uint32, uint64:
+			return ival.Uint() < jval.Uint()
+		case float32, float64:
+			return ival.Float() < jval.Float()
+		case string:
+			return ival.String() < jval.String()
+		}
+	}
+
+	return false
+}
 
 // SolrQuery holds a parsed query and query chain to run against the index. It's
 // called SolrQuery because the search queries use a subset of Solr's syntax.
@@ -39,7 +86,7 @@ type SolrQuery struct {
 
 // Search parses the given query string and search the given index for any
 // matching results.
-func Search(idx string, q string) ([]indexer.Indexable, error) {
+func Search(idx string, q string, rows int, sortOrder string, start int, partialData map[string]interface{}) ([]map[string]interface{}, error) {
 	/* Eventually we'll want more prep. To start, look right in the index */
 	query, qerr := url.QueryUnescape(q)
 	if qerr != nil {
@@ -59,9 +106,73 @@ func Search(idx string, q string) ([]indexer.Indexable, error) {
 	if err != nil {
 		return nil, err
 	}
-	results := solrQ.results()
-	objs := getResults(idx, results)
-	return objs, nil
+	qresults := solrQ.results()
+	objs := getResults(idx, qresults)
+	res := make([]map[string]interface{}, len(objs))
+	for i, r := range objs {
+		switch r := r.(type) {
+		case *client.Client:
+			jc := map[string]interface{}{
+				"name":       r.Name,
+				"chef_type":  r.ChefType,
+				"json_class": r.JSONClass,
+				"admin":      r.Admin,
+				"public_key": r.PublicKey(),
+				"validator":  r.Validator,
+			}
+			res[i] = jc
+		default:
+			res[i] = util.MapifyObject(r)
+		}
+	}
+
+	/* If we're doing partial search, tease out the fields we want. */
+	if partialData != nil {
+		res, err = PartialSearchFormat(res, partialData)
+		if err != nil {
+			return nil, err
+		}
+		for x, z := range res {
+			tmpRes := make(map[string]interface{})
+			switch ro := objs[x].(type) {
+			case *databag.DataBagItem:
+				dbiURL := fmt.Sprintf("/data/%s/%s", ro.DataBagName, ro.RawData["id"].(string))
+				tmpRes["url"] = util.CustomURL(dbiURL)
+			default:
+				tmpRes["url"] = util.ObjURL(objs[x].(util.GoiardiObj))
+			}
+			tmpRes["data"] = z
+
+			res[x] = tmpRes
+		}
+	}
+
+	// and at long last, sort
+	ss := strings.Split(sortOrder, " ")
+	sortKey := ss[0]
+	if sortKey == "id" {
+		sortKey = "name"
+	}
+	var ordering string
+	if len(ss) > 1 {
+		ordering = strings.ToLower(ss[1])
+	} else {
+		ordering = "asc"
+	}
+	sortResults := results{res, sortKey}
+	if ordering == "desc" {
+		sort.Sort(sort.Reverse(sortResults))
+	} else {
+		sort.Sort(sortResults)
+	}
+	res = sortResults.res
+
+	end := start + rows
+	if end > len(res) {
+		end = len(res)
+	}
+	res = res[start:end]
+	return res, nil
 }
 
 func (sq *SolrQuery) execute() (map[string]*indexer.IdxDoc, error) {
@@ -202,4 +313,100 @@ func getResults(variety string, toGet []string) []indexer.Indexable {
 		}
 	}
 	return results
+}
+
+func PartialSearchFormat(results []map[string]interface{}, partialFormat map[string]interface{}) ([]map[string]interface{}, error) {
+	/* regularize partial search keys */
+	psearchKeys := make(map[string][]string, len(partialFormat))
+	for k, v := range partialFormat {
+		switch v := v.(type) {
+		case []interface{}:
+			psearchKeys[k] = make([]string, len(v))
+			for i, j := range v {
+				switch j := j.(type) {
+				case string:
+					psearchKeys[k][i] = j
+				default:
+					err := fmt.Errorf("Partial search key %s badly formatted: %T %v", k, j, j)
+					return nil, err
+				}
+			}
+		case []string:
+			psearchKeys[k] = make([]string, len(v))
+			for i, j := range v {
+				psearchKeys[k][i] = j
+			}
+		default:
+			err := fmt.Errorf("Partial search key %s badly formatted: %T %v", k, v, v)
+			return nil, err
+		}
+	}
+	newResults := make([]map[string]interface{}, len(results))
+
+	for i, j := range results {
+		newResults[i] = make(map[string]interface{})
+		for key, vals := range psearchKeys {
+			var pval interface{}
+			/* The first key can either be top or first level.
+			 * Annoying, but that's how it is. */
+			if len(vals) > 0 {
+				if step, found := j[vals[0]]; found {
+					if len(vals) > 1 {
+						pval = walk(step, vals[1:])
+					} else {
+						pval = step
+					}
+				} else {
+					if len(vals) > 0 {
+						// bear in mind precedence. We need to
+						// overwrite later values with earlier
+						// ones.
+						keyRange := []string{"raw_data", "default", "default_attributes", "normal", "override", "override_attributes", "automatic"}
+						for _, r := range keyRange {
+							tval := walk(j[r], vals[0:])
+							if tval != nil {
+								switch pv := pval.(type) {
+								case map[string]interface{}:
+									// only merge if tval is also a map[string]interface{}
+									switch tval := tval.(type) {
+									case map[string]interface{}:
+										for g, h := range tval {
+											pv[g] = h
+										}
+										pval = pv
+									}
+								default:
+									pval = tval
+								}
+							}
+						}
+					}
+				}
+			}
+			newResults[i][key] = pval
+		}
+	}
+	return newResults, nil
+}
+
+func walk(v interface{}, keys []string) interface{} {
+	switch v := v.(type) {
+	case map[string]interface{}:
+		if _, found := v[keys[0]]; found {
+			if len(keys) > 1 {
+				return walk(v[keys[0]], keys[1:])
+			}
+			return v[keys[0]]
+		}
+		return nil
+	case map[string]string:
+		return v[keys[0]]
+	case map[string][]string:
+		return v[keys[0]]
+	default:
+		if len(keys) == 1 {
+			return v
+		}
+		return nil
+	}
 }
