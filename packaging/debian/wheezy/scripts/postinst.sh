d95b214e1bfaded2d7cd3c665731d0aff71279be
ctdk <jbingham@gmail.com>
2015-05-09 23:14:21 -0700

    Some packaging files for debian wheezy

---
+++ packaging/debian/wheezy/scripts/postinst.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -e
+
+[ -f /etc/default/goiardi ] && . /etc/default/goiardi
+
+if ! getent group "$group" > /dev/null 2>&1 ; then
+	addgroup --system "$group" --quiet
+fi
+if ! id $user > /dev/null 2>&1 ; then
+	adduser --system --home /var/lib/goiardi --no-create-home \
+		--ingroup "$group" --disabled-password --shell /bin/false \
+		"$user"
+fi
+chgrp goiardi /etc/goiardi
+chmod 775 /etc/goiardi
+
+if [ -x /bin/systemctl ]; then
+	/bin/systemctl daemon-reload
+	/bin/systemctl start goiardi.service
+else 
+	update-rc.d -f goiardi defaults
+	invoke-rc.d goiardi start
+fi
