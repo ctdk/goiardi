ae6255f8c976d0e8591cde36874b92bb170920d6
ctdk <jbingham@gmail.com>
2015-11-07 11:06:43 -0800

    Update version number in preparation for a release

--- config/config.go
+++ config/config.go
@@ -173,7 +173,7 @@ type Options struct {
 }
 
 // The goiardi version.
-const Version = "0.10.1"
+const Version = "0.10.2"
 
 // The chef version we're at least aiming for, even if it's not complete yet.
 const ChefVersion = "11.1.7"
