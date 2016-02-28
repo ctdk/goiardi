834652eb34c52a3c615bdb3ca21b7a88205a34df
ctdk <jbingham@gmail.com>
2016-02-28 15:15:59 -0800

    copyright info, comment/doc search metrics

--- search/metrics.go
+++ search/metrics.go
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2013-2016, Jeremy Bingham (<jbingham@gmail.com>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Functions and vars for search timing metrics in statsd.
+
 package search
 
 import (
@@ -11,6 +29,7 @@ import (
 var inMemSearchTimings met.Timer
 var pgSearchTimings met.Timer
 
+// InitializeMetrics initializes the statsd timers for search queries.
 func InitializeMetrics(metricsBackend met.Backend) {
 	inMemSearchTimings = metricsBackend.NewTimer("search.in_mem", 0)
 	pgSearchTimings = metricsBackend.NewTimer("search.pg", 0)
