2df8b3b6daad717ce8e5fd3dc67b2a7fd849855b
ctdk <jbingham@gmail.com>
2016-11-22 14:21:32 -0800

    bump version again after the vendoring

--- config/config.go
+++ config/config.go
@@ -204,7 +204,7 @@ type Options struct {
 }
 
 // The goiardi version.
-const Version = "0.11.1-pre1"
+const Version = "0.11.1-pre2"
 
 // The chef version we're at least aiming for, even if it's not complete yet.
 const ChefVersion = "11.1.7"
