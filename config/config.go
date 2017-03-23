5fd86b292acde35e0ff45c344ef60ef542f6285d
ctdk <jbingham@gmail.com>
2017-03-23 15:47:14 -0700

    bump version, hopefully last one before a new release

--- config/config.go
+++ config/config.go
@@ -212,7 +212,7 @@ type Options struct {
 }
 
 // The goiardi version.
-const Version = "0.11.3-pre3"
+const Version = "0.11.3-pre4"
 
 // The chef version we're at least aiming for, even if it's not complete yet.
 const ChefVersion = "11.1.7"
