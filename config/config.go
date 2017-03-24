48d6b0d70bb34fe73d5d5ef4034c5a8f1c315b7e
ctdk <jbingham@gmail.com>
2017-03-23 22:19:49 -0700

    set version in config/config.go

--- config/config.go
+++ config/config.go
@@ -212,7 +212,7 @@ type Options struct {
 }
 
 // The goiardi version.
-const Version = "0.11.3-pre4"
+const Version = "0.11.3"
 
 // The chef version we're at least aiming for, even if it's not complete yet.
 const ChefVersion = "11.1.7"
