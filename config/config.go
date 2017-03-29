2bd1574170238cf50f5bbb7aad824223dec7e5da
ctdk <jbingham@gmail.com>
2017-03-29 09:05:12 -0700

    Revert "the version number shouldn't have been bumped for this branch"
    This reverts commit 681e4e463fd18bcb4555803545d4b115bca1467c.

--- config/config.go
+++ config/config.go
@@ -204,7 +204,7 @@ type Options struct {
 }
 
 // The goiardi version.
-const Version = "0.11.2."
+const Version = "0.11.3-pre1"
 
 // The chef version we're at least aiming for, even if it's not complete yet.
 const ChefVersion = "11.1.7"
