1ea28ea0699c57b40003f2bf4727a363f88b8498
ctdk <jbingham@gmail.com>
2019-05-31 10:11:19 -0700

    start filling in the group SQL bits, but that needs set aside until I look up array aggregate syntax

---
+++ group/sql_funcs.go
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2013-2019, Jeremy Bingham (<jbingham@gmail.com>)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package group
+
+// SQL goodies for groups
+
+// Arrrgh, that's right. I need to look up the selecting an array aggregate with
+// table join and so forth for groups
