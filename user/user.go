3c5b783c866875deef7562a7c6a3539d5ce37273
ctdk <jbingham@gmail.com>
2019-08-09 09:22:31 -0700

    user not found error message inexplicably referred to clients

--- user/user.go
+++ user/user.go
@@ -127,7 +127,7 @@ func Get(name string) (*User, util.Gerror) {
 				gerr = util.Errorf(err.Error())
 				gerr.SetStatus(http.StatusInternalServerError)
 			} else {
-				gerr = util.Errorf("Client %s not found", name)
+				gerr = util.Errorf("User %s not found", name)
 				gerr.SetStatus(http.StatusNotFound)
 			}
 			return nil, gerr
