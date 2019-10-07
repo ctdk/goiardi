847b8dc3158e460c95dc821d1416de9c5c6daed4
ctdk <jbingham@gmail.com>
2019-10-07 08:45:33 -0700

    So very, very minor, but bump the copyright dates in the docs. Chances are good that they'll need to be bumped again before this actually gets out.

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'goiardi'
-copyright = u'2014-2017, Jeremy Bingham'
+copyright = u'2014-2018, Jeremy Bingham'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
