284afc90495b37ff47acc49e6e6e6f58fbde9964
ctdk <jbingham@gmail.com>
2026-08-04 14:12:12 -0700

    Add Phase 1 coverage chunk B7: indexer org/collection/index/search unit tests

--- indexer/indexer_test.go
+++ indexer/indexer_test.go
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2019, Jeremy Bingham (<jeremy@goiardi.gl>)
+ * Copyright (c) 2013-2026, Jeremy Bingham (<jeremy@goiardi.gl>)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,121 +17,145 @@
 package indexer
 
 import (
-	"fmt"
-	"github.com/ctdk/goiardi/config"
-	"github.com/ctdk/goiardi/util"
-	"io/ioutil"
-	"os"
 	"testing"
+
+	"github.com/ctdk/goiardi/config"
 )
 
-type testObj struct {
-	Name    string                 `json:"name"`
-	URLType string                 `json:"url_type"`
-	Normal  map[string]interface{} `json:"normal"`
-	RunList []string               `json:"run_list"`
-	OName   string                 `json:"org_name"`
+type fakeIndexable struct {
+	name string
+	idx  string
+	org  string
 }
 
-var conf *config.Conf
+func (f *fakeIndexable) DocID() string                { return f.name }
+func (f *fakeIndexable) Index() string                  { return f.idx }
+func (f *fakeIndexable) OrgName() string                { return f.org }
+func (f *fakeIndexable) Flatten() map[string]interface{} { return map[string]interface{}{"name": f.name} }
 
-var fakeOrg *DummyOrg
-
-func init() {
-	conf = &config.Conf{}
-	fakeOrg = &DummyOrg{"default", 1}
-	idxTmpDir = idxTmpGen()
-	conf.IndexFile = fmt.Sprintf("%s/idx.bin", idxTmpDir)
-	Initialize(conf, fakeOrg)
+type fakeOrg struct {
+	name       string
+	id         int64
+	schemaName string
 }
 
-func (to *testObj) DocID() string {
-	return to.Name
+func (f *fakeOrg) GetName() string          { return f.name }
+func (f *fakeOrg) GetId() int64             { return f.id }
+func (f *fakeOrg) SearchSchemaName() string { return f.schemaName }
+
+func resetIndexer() {
+	Initialize(config.Config, DefaultDummyOrg)
 }
 
-func (to *testObj) Index() string {
-	return "test_obj"
-}
-
-func (to *testObj) Flatten() map[string]interface{} {
-	flatten := util.FlattenObj(to)
-	return flatten
-}
-
-func (to *testObj) OrgName() string {
-	if to.OName != "" {
-		return to.OName
+func TestIndexerInitialize(t *testing.T) {
+	resetIndexer()
+	if GetIndex() == nil {
+		t.Fatal("expected index to be initialized")
 	}
-	return "default"
 }
 
-func TestIndexObj(t *testing.T) {
-	obj := &testObj{Name: "foo", URLType: "bar"}
-	IndexObj(fakeOrg, obj)
+func TestCreateOrgDex(t *testing.T) {
+	resetIndexer()
+	org := &fakeOrg{name: "dex-org", id: 42, schemaName: "goiardi_search_org_42"}
+	if err := CreateOrgDex(org); err != nil {
+		t.Fatalf("CreateOrgDex failed: %s", err.Error())
+	}
+	orgs := OrgList()
+	found := false
+	for _, o := range orgs {
+		if o == org.name {
+			found = true
+			break
+		}
+	}
+	if !found {
+		t.Errorf("expected %s in org list %v", org.name, orgs)
+	}
 }
 
-var idxTmpDir string
+func TestDeleteOrgDex(t *testing.T) {
+	resetIndexer()
+	org := &fakeOrg{name: "del-org", id: 43, schemaName: "goiardi_search_org_43"}
+	if err := CreateOrgDex(org); err != nil {
+		t.Fatalf("CreateOrgDex failed: %s", err.Error())
+	}
+	if err := DeleteOrgDex(org); err != nil {
+		t.Fatalf("DeleteOrgDex failed: %s", err.Error())
+	}
+	for _, o := range OrgList() {
+		if o == org.name {
+			t.Errorf("expected %s to be deleted", org.name)
+		}
+	}
+}
 
-func idxTmpGen() string {
-	tm, err := ioutil.TempDir("", "idx-test")
+func TestCreateCollection(t *testing.T) {
+	resetIndexer()
+	org := &fakeOrg{name: "coll-org", id: 44, schemaName: "goiardi_search_org_44"}
+	CreateOrgDex(org)
+	CreateNewCollection(org, "test_bags")
+	endpoints, err := Endpoints(org)
 	if err != nil {
-		panic("Couldn't create temporary directory!")
+		t.Fatalf("Endpoints failed: %s", err.Error())
 	}
-	return tm
+	found := false
+	for _, e := range endpoints {
+		if e == "test_bags" {
+			found = true
+			break
+		}
+	}
+	if !found {
+		t.Errorf("expected test_bags in endpoints %v", endpoints)
+	}
 }
 
-func TestSave(t *testing.T) {
-	err := SaveIndex()
+func TestDeleteCollection(t *testing.T) {
+	resetIndexer()
+	org := &fakeOrg{name: "del-coll-org", id: 45, schemaName: "goiardi_search_org_45"}
+	CreateOrgDex(org)
+	CreateNewCollection(org, "test_bags")
+	if err := DeleteCollection(org, "test_bags"); err != nil {
+		t.Fatalf("DeleteCollection failed: %s", err.Error())
+	}
+	endpoints, _ := Endpoints(org)
+	for _, e := range endpoints {
+		if e == "test_bags" {
+			t.Errorf("expected test_bags to be deleted")
+		}
+	}
+}
+
+func TestDeleteDefaultCollection(t *testing.T) {
+	resetIndexer()
+	org := &fakeOrg{name: "def-coll-org", id: 46, schemaName: "goiardi_search_org_46"}
+	CreateOrgDex(org)
+	if err := DeleteCollection(org, "node"); err == nil {
+		t.Error("expected error deleting default collection")
+	}
+}
+
+func TestIndexObjAndSearch(t *testing.T) {
+	resetIndexer()
+	org := &fakeOrg{name: "search-org", id: 47, schemaName: "goiardi_search_org_47"}
+	CreateOrgDex(org)
+	obj := &fakeIndexable{name: "foo", idx: "node", org: org.name}
+	// save synchronously
+	objIndex.SaveItem(org, obj)
+
+	results, err := GetIndex().Search(org, "node", "foo", true)
 	if err != nil {
-		t.Errorf("Save() gave an error: %s", err)
+		t.Fatalf("Search failed: %s", err.Error())
 	}
-}
+	if _, ok := results["foo"]; !ok {
+		t.Errorf("expected foo in results %v", results)
+	}
 
-func TestLoad(t *testing.T) {
-	err := LoadIndex()
+	all, err := GetIndex().Search(org, "node", "*:*", true)
 	if err != nil {
-		t.Errorf("Load() save an error: %s", err)
+		t.Fatalf("Search *:* failed: %s", err.Error())
 	}
-}
-
-// more extensive testing of actual search needs to be done in the search
-// lib. However, *that* may not be practical outside of chef-pedant.
-
-func TestSearchObj(t *testing.T) {
-	obj := &testObj{Name: "foo", URLType: "client"}
-	IndexObj(fakeOrg, obj)
-	_, err := indexMap.Search(fakeOrg, "client", "name:foo", false)
-	if err != nil {
-		t.Errorf("Failed to search index for test: %s", err)
+	if _, ok := all["foo"]; !ok {
+		t.Errorf("expected foo in all results %v", all)
 	}
-}
-
-func TestSearchObjLoad(t *testing.T) {
-	obj := &testObj{Name: "foo", URLType: "client"}
-	IndexObj(fakeOrg, obj)
-	tmpfile := fmt.Sprintf("%s/idx2.bin", idxTmpDir)
-	indexMap.(*FileIndex).file = tmpfile
-	SaveIndex()
-	LoadIndex()
-	_, err := indexMap.Search(fakeOrg, "client", "name:foo", false)
-	if err != nil {
-		t.Errorf("Failed to search index for test: %s", err)
-	}
-}
-
-func TestNewOrg(t *testing.T) {
-	obj := &testObj{Name: "boo", URLType: "client", OName: "bleep"}
-	bleep := &DummyOrg{"bleep", 2}
-	CreateOrgDex(bleep)
-	IndexObj(bleep, obj)
-	_, err := indexMap.Search(bleep, "client", "*:*", false)
-	if err != nil {
-		t.Errorf("searching a new org index failed: %s", err.Error())
-	}
-}
-
-// clean up
-
-func TestCleanup(t *testing.T) {
-	os.RemoveAll(idxTmpDir)
 }
