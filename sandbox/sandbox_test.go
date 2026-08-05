fee6d68515d7e69b57d142631f5766be9141236b
ctdk <jbingham@gmail.com>
2026-08-04 20:05:20 -0700

    Add Phase 1 coverage chunk F: sandbox unit tests
    - Add tests for New, invalid checksum, Save/Get, Delete, GetList,
      AllSandboxes, UploadChkList, and IsComplete.
    - Register filestore.FileStore gob type in sandbox_test.go init.

--- sandbox/sandbox_test.go
+++ sandbox/sandbox_test.go
@@ -17,16 +17,22 @@
 package sandbox
 
 import (
+	"bytes"
 	"crypto/md5"
 	"encoding/gob"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"testing"
+	"time"
+
 	"github.com/ctdk/goiardi/config"
 	"github.com/ctdk/goiardi/fakeacl"
+	"github.com/ctdk/goiardi/filestore"
 	"github.com/ctdk/goiardi/indexer"
 	"github.com/ctdk/goiardi/organization"
 	"math/rand"
-	"testing"
-	"time"
 )
 
 const (
@@ -48,6 +54,7 @@ const (
 
 func init() {
 	indexer.Initialize(config.Config, indexer.DefaultDummyOrg)
+	gob.Register(new(filestore.FileStore))
 }
 
 func randStringBytesMaskImprSrc(n int) string {
@@ -155,4 +162,163 @@ func TestSandboxPurgeWith30(t *testing.T) {
 		t.Errorf("After purging there should have been 24 sandboxes, but there were %d instead", len(all))
 	}
 
+}
+
+func setupSandboxTest(t *testing.T) (*organization.Organization, func()) {
+	t.Helper()
+	tmpDir, err := ioutil.TempDir("", "sandbox-test")
+	if err != nil {
+		t.Fatalf("failed to create temp dir: %s", err.Error())
+	}
+	config.Config.PolicyRoot = tmpDir
+	o, gerr := organization.New("sandbox-test-"+t.Name(), "Sandbox Test")
+	if gerr != nil {
+		t.Fatalf("failed to create org: %s", gerr.Error())
+	}
+	fakeacl.LoadFakeACL(o)
+	if err := o.Save(); err != nil {
+		t.Fatalf("failed to save org: %s", err.Error())
+	}
+	indexer.Initialize(config.Config, o)
+	return o, func() { os.RemoveAll(tmpDir) }
+}
+
+func checksum(data []byte) string {
+	h := md5.New()
+	h.Write(data)
+	return fmt.Sprintf("%x", h.Sum(nil))
+}
+
+func makeReadCloser(data []byte) io.ReadCloser {
+	return io.NopCloser(bytes.NewReader(data))
+}
+
+func TestSandboxNew(t *testing.T) {
+	org, cleanup := setupSandboxTest(t)
+	defer cleanup()
+
+	chk := checksum([]byte("test-data"))
+	sb, err := New(org, map[string]interface{}{chk: nil})
+	if err != nil {
+		t.Fatalf("New failed: %s", err.Error())
+	}
+	if sb.ID == "" {
+		t.Error("expected sandbox ID to be set")
+	}
+	if len(sb.Checksums) != 1 || sb.Checksums[0] != chk {
+		t.Errorf("expected checksums [%s], got %v", chk, sb.Checksums)
+	}
+}
+
+func TestSandboxNewInvalidChecksum(t *testing.T) {
+	org, cleanup := setupSandboxTest(t)
+	defer cleanup()
+
+	if _, err := New(org, map[string]interface{}{"not-hex!!": nil}); err == nil {
+		t.Error("expected error for invalid checksum")
+	}
+}
+
+func TestSandboxSaveAndGet(t *testing.T) {
+	org, cleanup := setupSandboxTest(t)
+	defer cleanup()
+
+	chk := checksum([]byte("test-data"))
+	sb, _ := New(org, map[string]interface{}{chk: nil})
+	if err := sb.Save(); err != nil {
+		t.Fatalf("Save failed: %s", err.Error())
+	}
+	got, err := Get(org, sb.ID)
+	if err != nil {
+		t.Fatalf("Get failed: %s", err.Error())
+	}
+	if got.ID != sb.ID {
+		t.Errorf("expected %s, got %s", sb.ID, got.ID)
+	}
+}
+
+func TestSandboxDelete(t *testing.T) {
+	org, cleanup := setupSandboxTest(t)
+	defer cleanup()
+
+	chk := checksum([]byte("test-data"))
+	sb, _ := New(org, map[string]interface{}{chk: nil})
+	sb.Save()
+	if err := sb.Delete(); err != nil {
+		t.Fatalf("Delete failed: %s", err.Error())
+	}
+	if _, err := Get(org, sb.ID); err == nil {
+		t.Error("expected error after deleting sandbox")
+	}
+}
+
+func TestSandboxGetListAndAllSandboxes(t *testing.T) {
+	org, cleanup := setupSandboxTest(t)
+	defer cleanup()
+
+	chk1 := checksum([]byte("one"))
+	chk2 := checksum([]byte("two"))
+	sb1, _ := New(org, map[string]interface{}{chk1: nil})
+	sb2, _ := New(org, map[string]interface{}{chk2: nil})
+	sb1.Save()
+	sb2.Save()
+
+	list := GetList(org)
+	if len(list) != 2 {
+		t.Errorf("expected 2 sandboxes in list, got %d", len(list))
+	}
+	all := AllSandboxes(org)
+	if len(all) != 2 {
+		t.Errorf("expected 2 sandboxes from AllSandboxes, got %d", len(all))
+	}
+}
+
+func TestSandboxUploadChkList(t *testing.T) {
+	org, cleanup := setupSandboxTest(t)
+	defer cleanup()
+
+	data := []byte("uploaded")
+	chk := checksum(data)
+	other := checksum([]byte("missing"))
+	fs, err := filestore.New(org, chk, makeReadCloser(data), int64(len(data)))
+	if err != nil {
+		t.Fatalf("failed to create filestore item: %s", err.Error())
+	}
+	if err := fs.Save(); err != nil {
+		t.Fatalf("failed to save filestore item: %s", err.Error())
+	}
+
+	sb, _ := New(org, map[string]interface{}{chk: nil, other: nil})
+	stats := sb.UploadChkList()
+	if stats[chk]["needs_upload"] != false {
+			t.Errorf("expected existing checksum not to need upload")
+	}
+	if stats[other]["needs_upload"] != true {
+			t.Errorf("expected missing checksum to need upload")
+	}
+}
+
+func TestSandboxIsComplete(t *testing.T) {
+	org, cleanup := setupSandboxTest(t)
+	defer cleanup()
+
+	data := []byte("complete")
+	chk := checksum(data)
+	fs, err := filestore.New(org, chk, makeReadCloser(data), int64(len(data)))
+	if err != nil {
+		t.Fatalf("failed to create filestore item: %s", err.Error())
+	}
+	if err := fs.Save(); err != nil {
+		t.Fatalf("failed to save filestore item: %s", err.Error())
+	}
+
+	sb, _ := New(org, map[string]interface{}{chk: nil})
+	if err := sb.IsComplete(); err != nil {
+		t.Fatalf("IsComplete failed for uploaded checksum: %s", err.Error())
+	}
+
+	sb2, _ := New(org, map[string]interface{}{checksum([]byte("missing")): nil})
+	if err := sb2.IsComplete(); err == nil {
+		t.Error("expected IsComplete to fail when checksum is missing")
+	}
 }
